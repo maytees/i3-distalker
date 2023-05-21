595109cc7c0d8e5ce2bdc9edab11145126449fa5
maytees <mateespublicprofile@gmail.com>
2023-05-21 19:11:05 -0400

    replaced old lib

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,16 @@
-extern crate discord_rpc_client;
-use discord_rpc_client::Client;
+use activity::{Assets, Button};
+use discord_rich_presence::{activity, DiscordIpc, DiscordIpcClient};
 use i3_ipc::{
     event::{Event, Subscribe},
     I3Stream,
 };
-use std::{env, io, thread, time};
+use std::{error::Error, io};
 
-fn main() -> io::Result<()> {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut i3 = I3Stream::conn_sub(&[Subscribe::Window, Subscribe::Workspace])?;
 
-    let mut drpc = Client::new(1109944637281550457);
-    drpc.start();
+    let mut client = DiscordIpcClient::new("1109944637281550457")?;
+    client.connect()?;
 
     for e in i3.listen() {
         match e? {
@@ -32,39 +32,24 @@ fn main() -> io::Result<()> {
 
                 let msg = format!("{} - {}", name, class);
 
-                match drpc.set_activity(|act| {
-                    act.state(msg)
-                        .details(
-                            "What you see below is what I am currently doing. You Weird stalker...",
-                        )
-                        .assets(|assets| assets.large_image("troll").large_text("vtest"))
-                }) {
-                    Ok(_) => {}
-                    Err(e) => println!("{}", e),
-                }
+                let payload = activity::Activity::new()
+                    .details("What you see below is what window I am currently on.")
+                    .state(msg.as_str())
+                    .assets(
+                        Assets::new()
+                            .large_image("https://vikasbackend.matees.net/9836")
+                            .large_text("Go away stalker"),
+                    )
+                    .buttons(vec![Button::new(
+                        "View stalk(-app?) repository",
+                        "https://github.com/maytees/i3-distalker",
+                    )]);
+                client.set_activity(payload)?;
             }
-            _ => continue,
+            _ => {}
         }
     }
 
-    println!("bye");
+    client.close()?;
     Ok(())
 }
-
-// fn rpc() {
-//     // Get our main status message
-//     let state_message = env::args().nth(1).expect("Requires at least one argument");
-
-//     // Create the client
-//     let mut drpc = Client::new(1109944637281550457);
-
-//     // Start up the client connection, so that we can actually send and receive stuff
-//     drpc.start();
-
-//     // Set the activity
-//     drpc.set_activity(|act| act.state(state_message))
-//         .expect("Failed to set activity");
-
-//     // Wait 10 seconds before exiting
-//     thread::sleep(time::Duration::from_secs(10));
-// }
